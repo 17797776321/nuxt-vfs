0e3d55057eaa526c3e5cbca71d9780412713702d
张宸 <531336750@qq.com>
2019-01-03 17:03:44 +0800

    banner图上得文字

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,8 @@
       <img 
         :src="banner" 
         alt="">
+      <div class="tit">一站式服务  助力企业转型</div>
+      <div class="des">十大行业解决方案  数百款营销活动 公众号</div>
     </div>
   </section>
 </template>
@@ -25,9 +27,28 @@ export default {
   padding: 80px 0 0 0;
   .banner_box {
     width: 100%;
+    position: relative;
     img {
       display: block;
       width: 100%;
+    }
+    .tit {
+      width: 100%;
+      font-size: 40px;
+      color: #ffffff;
+      text-align: center;
+      line-height: 60px;
+      position: absolute;
+      top: 222px;
+    }
+    .des {
+      width: 100%;
+      font-size: 16px;
+      color: #ffffff;
+      text-align: center;
+      line-height: 48px;
+      position: absolute;
+      top: 278px;
     }
   }
 }
